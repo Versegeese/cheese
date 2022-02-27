ff02db18c6123c87417befef7373d0109daaf258
jorgep31415 <32918197+jorgep31415@users.noreply.github.com>
2022-02-26 21:16:49 -0800

    Set workspace settings to format with eslint, prettier and use debugger

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Cheese
 
 The long anticipated sequel to [Geese Game](https://versegeese.github.io/geese-game/)!
+
+For development, install the following VSCode extensions:
+
+- [Debugger for Chrome](https://marketplace.visualstudio.com/items?itemName=msjsdiag.debugger-for-chrome)
+- [ESLint](https://marketplace.visualstudio.com/items?itemName=dbaeumer.vscode-eslint)
+- [Prettier](https://marketplace.visualstudio.com/items?itemName=esbenp.prettier-vscode)
--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import * as THREE from 'three';
-import React, { useRef, useState } from 'react';
-import { Canvas, useFrame } from '@react-three/fiber';
-import './App.css';
+import * as THREE from "three";
+import React, { useRef, useState } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import "./App.css";
 
-function Box(props: JSX.IntrinsicElements['mesh']) {
+function Box(props: JSX.IntrinsicElements["mesh"]) {
   // This reference will give us direct access to the THREE.Mesh object
-  const ref = useRef<THREE.Mesh>(null!)
+  const ref = useRef<THREE.Mesh>(null!);
   // Hold state for hovered and clicked events
-  const [hovered, hover] = useState(false)
-  const [clicked, click] = useState(false)
+  const [hovered, hover] = useState(false);
+  const [clicked, click] = useState(false);
   // Rotate mesh every frame, this is outside of React without overhead
-  useFrame((state, delta) => (ref.current.rotation.x += 0.01))
+  useFrame((state, delta) => (ref.current.rotation.x += 0.01));
 
   return (
     <mesh
@@ -19,11 +19,12 @@ function Box(props: JSX.IntrinsicElements['mesh']) {
       scale={clicked ? 1.5 : 1}
       onClick={(event) => click(!clicked)}
       onPointerOver={(event) => hover(true)}
-      onPointerOut={(event) => hover(false)}>
+      onPointerOut={(event) => hover(false)}
+    >
       <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
     </mesh>
-  )
+  );
 }
 
 export default function App() {
@@ -34,5 +35,5 @@ export default function App() {
       <Box position={[-1.2, 0, 0]} />
       <Box position={[1.2, 0, 0]} />
     </Canvas>
-  )
+  );
 }
